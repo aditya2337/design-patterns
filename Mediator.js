920d762aabcee55c95d153b736cc84e3b9ab3e09
aditya <androiditya@gmail.com>
2021-08-18 12:30:26 +0530

    Added mediator pattern

---
+++ Mediator.js
@@ -0,0 +1,42 @@
+const mediator = (function() {
+  const channels = {}
+
+  const subscribe = function(channel, fn) {
+    if (!channels[channel]) {
+      channels[channel] = []
+    }
+
+    channels[channel].push({ context: this, callback: fn })
+    return this
+  }
+
+  const publish = function(channel, ...args) {
+    if (!channels[channel]) {
+      return false
+    }
+
+    
+    channels[channel].forEach(subscriber => {
+      subscriber.callback.apply(subscriber.context, args)
+    })
+
+    return this
+  }
+
+  return {
+    publish,
+    subscribe,
+    installTo: (obj) => {
+      obj.subscribe = subscribe
+      obj.publish = publish
+    }
+  }
+})()
+
+const person = "GV"
+
+mediator.subscribe('nameChange', (args) => {
+  console.log(args)
+})
+
+mediator.publish('nameChange', 'Aditya')
